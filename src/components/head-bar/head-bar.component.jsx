cad402586dcbc0f3f0ba4e9d64ff8e3de579fe8d
Emily Yu <bluerainmango@gmail.com>
2020-06-27 01:49:33 -0700

    created empty head-bar, header components

---
+++ src/components/head-bar/head-bar.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./head-bar.styles.scss";
+
+export default function HeadBar() {
+  return (
+    <div>
+      <p className="headbar">FREE SHIPPING ON ALL ORDERS OVER $75</p>
+    </div>
+  );
+}
