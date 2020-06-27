20e917db918a77b1cd9b0841e8f932ea1def8528
Emily Yu <bluerainmango@gmail.com>
2020-06-27 02:58:46 -0700

    added fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "./fonts/pacifico-font/Pacifico.ttf";
+import "./fonts/lato/Lato-Thin.ttf";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
