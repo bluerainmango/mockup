ab582ed11383d2b86adcd76550f8002d83ec4387
Emily Yu <bluerainmango@gmail.com>
2020-06-27 16:11:58 -0700

    added pages > homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 // import logo from "./logo.svg";
-import HeadBar from "./components/head-bar/head-bar.component";
-import Header from "./components/header/header.component";
+import Homepage from "./page/homepage.component";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <HeadBar />
-      <Header />
+      <Homepage />
     </div>
   );
 }
---
+++ src/page/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeadBar from "../components/head-bar/head-bar.component";
+import Header from "../components/header/header.component";
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <HeadBar />
+      <Header />
+    </div>
+  );
+}
+
+export default Homepage;
