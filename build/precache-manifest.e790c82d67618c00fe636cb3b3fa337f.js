4fc57b741c623d5bd0b0ff632dafaa8e0f69675e
Emily Yu <bluerainmango@gmail.com>
2020-06-29 05:07:34 -0700

    react build

---
+++ build/precache-manifest.e790c82d67618c00fe636cb3b3fa337f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb3e5b58564ff2665626925be103a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164ff928f6bd50fb399b",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "34c67f92af0b792cddfa",
+    "url": "/static/css/main.1042b14b.chunk.css"
+  },
+  {
+    "revision": "164ff928f6bd50fb399b",
+    "url": "/static/js/2.e6141ec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6141ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c67f92af0b792cddfa",
+    "url": "/static/js/main.cd17e907.chunk.js"
+  },
+  {
+    "revision": "10f468c686498cc3bc2c",
+    "url": "/static/js/runtime-main.18dc98a8.js"
+  },
+  {
+    "revision": "90e1d3559ac52f7f0f77a86e1bfd632d",
+    "url": "/static/media/Lato-Light.90e1d355.ttf"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/static/media/Lato-Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "3c6cfb1aebd888a0eb4c8fba94140fa6",
+    "url": "/static/media/Lato-Semibold.3c6cfb1a.ttf"
+  },
+  {
+    "revision": "eb1635403cd764912ca1e0af78735797",
+    "url": "/static/media/Lato-Thin.eb163540.ttf"
+  },
+  {
+    "revision": "6b6170fe52fb23f505b4e056fefd2679",
+    "url": "/static/media/Pacifico.6b6170fe.ttf"
+  },
+  {
+    "revision": "b281bf6ba680e2636da446a56dbbb193",
+    "url": "/static/media/accessories-image-mobile.b281bf6b.jpg"
+  },
+  {
+    "revision": "3181decb286654080050e64a20ee4979",
+    "url": "/static/media/accessories-image.3181decb.jpg"
+  },
+  {
+    "revision": "79e861a65b21a00844ba365b177b4e24",
+    "url": "/static/media/hero-image-1-mobile.79e861a6.jpg"
+  },
+  {
+    "revision": "b42a7ccd945f933be15eecdf1287d77d",
+    "url": "/static/media/hero-image-1.b42a7ccd.jpg"
+  },
+  {
+    "revision": "cfaec045aa46f59d742faf9355cc9c42",
+    "url": "/static/media/hero-image-2-mobile.cfaec045.jpg"
+  },
+  {
+    "revision": "3adcc50f25c8ecb22d91cef8d5bd6aed",
+    "url": "/static/media/hero-image-2.3adcc50f.jpg"
+  },
+  {
+    "revision": "21572e5076f2d4821b58740fa90d58b1",
+    "url": "/static/media/mens-image.21572e50.jpg"
+  },
+  {
+    "revision": "73b1f38046430359a7f7944ff6c23c51",
+    "url": "/static/media/product-image-1.73b1f380.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "2637bbe60385449e43773f59f6ba6eec",
+    "url": "/static/media/social-1.2637bbe6.jpg"
+  },
+  {
+    "revision": "a79eb98879467339b9c708c31b0f416e",
+    "url": "/static/media/social-2.a79eb988.jpg"
+  },
+  {
+    "revision": "4f51799f30d4f85ebf85f9eab37d5372",
+    "url": "/static/media/social-3.4f51799f.jpg"
+  },
+  {
+    "revision": "7e7f4aa4af8979a1171500a952c437b9",
+    "url": "/static/media/social-4.7e7f4aa4.jpg"
+  },
+  {
+    "revision": "d017f63f42a13ef2b01715f019c3f971",
+    "url": "/static/media/social-5.d017f63f.jpg"
+  },
+  {
+    "revision": "c10f625511647413250d7b056e6f0d37",
+    "url": "/static/media/womens-image.c10f6255.jpg"
+  }
+]);
