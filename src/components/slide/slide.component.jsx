449a8a30ffdf7bf611a293bce1b92667c808be15
Emily Yu <bluerainmango@gmail.com>
2020-06-29 04:47:35 -0700

    fixed btn's a hover color bug

--- src/components/slide/slide.component.jsx
+++ src/components/slide/slide.component.jsx
@@ -77,7 +77,7 @@ export default function Slide(props) {
       ssr={true}
       infinite={true}
       autoPlay={true}
-      autoPlaySpeed={1000}
+      autoPlaySpeed={10000}
       keyBoardControl={true}
       customTransition="all .5"
       transitionDuration={500}
