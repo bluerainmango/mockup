54e5199d934ae8e585acd00220e4b298d0ef3fa8
Emily Yu <bluerainmango@gmail.com>
2020-06-29 04:01:31 -0700

    added mobile header & togglable mobile menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
+import logo from "../../assets/images/logo.png";
+import hamburger from "../../assets/images/hamburger-icon.png";
+import cart from "../../assets/images/cart.png";
 
+import menus from "./header.data";
+import listGenerator from "../utils/list-generator";
 import "./header.styles.scss";
 
 export default function Header() {
+  const mobileMenu = useRef();
+
+  const handleClickClose = (e) => {
+    mobileMenu.current.classList.add("hidden");
+  };
+
+  const handleClickShow = (e) => {
+    console.log(
+      "🍔 clicked burger menu: ",
+      e.target,
+      mobileMenu.current.classList
+    );
+    mobileMenu.current.classList.remove("hidden");
+  };
+
   return (
     <nav className="menu">
+      <div className="menu__list--mobile hidden" ref={mobileMenu}>
+        <a href="#" className="close" onClick={handleClickClose}>
+          &times;
+        </a>
+        <ul>{menus.map((menu, i) => listGenerator(menu))}</ul>
+      </div>
+      <img
+        className="menu__burger"
+        src={hamburger}
+        alt="hamgurger-menu-icon"
+        onClick={handleClickShow}
+      />
       <ul className="menu__list">
         <li>
-          <a href="#">Logo</a>
+          <a href="#">
+            <img src={logo} alt="logo" />
+          </a>
         </li>
-        <li>
-          <a href="#">Women</a>
-        </li>
-        <li>
-          <a href="#">Mens</a>
-        </li>
-        <li>
-          <a href="#">Accessories</a>
-        </li>
-        <li>
-          <a href="#">Sale!</a>
-        </li>
+        {menus.map((menu, i) => listGenerator(menu))}
       </ul>
+      <img className="menu__cart" src={cart} alt="cart" />
     </nav>
   );
 }
---
+++ src/components/header/header.data.js
@@ -0,0 +1,8 @@
+const menus = [
+  { name: "Womens", url: "#" },
+  { name: "Mens", url: "#" },
+  { name: "Accessories", url: "#" },
+  { name: "Sale!", url: "#" },
+];
+
+export default menus;
